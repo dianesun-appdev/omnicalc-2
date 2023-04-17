c4afb4c50ec790563af06b2157304b5a270f3ff2
sun-diane Sun <dianesun1@gmail.com>
2023-04-17 20:30:41 +0000

    First commit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,49 @@
 class ApplicationController < ActionController::Base
+  def input_add
+    render({ :template => "calculator/input_form_add.html.erb" })
+  end
 
+  def input_subtract
+    render({ :template => "calculator/input_form_subtract.html.erb" })
+  end
+
+  def input_multiply
+    render({ :template => "calculator/input_form_multiply.html.erb" })
+  end
+
+  def input_divide
+    render({ :template => "calculator/input_form_divide.html.erb" })
+  end
+
+  def wizard_add
+    @first_num = params[:first_num].to_f
+    @second_num = params[:second_num].to_f
+    @result = @first_num + @second_num
+
+    render({ :template => "calculator/results_add.html.erb" })
+  end
+
+  def wizard_subtract
+    @first_num = params[:first_num].to_f
+    @second_num = params[:second_num].to_f
+    @result = @second_num - @first_num
+
+    render({ :template => "calculator/results_subtract.html.erb" })
+  end
+
+  def wizard_multiply
+    @first_num = params[:first_num].to_f
+    @second_num = params[:second_num].to_f
+    @result = @first_num * @second_num
+
+    render({ :template => "calculator/results_multiply.html.erb" })
+  end
+
+  def wizard_divide
+    @first_num = params[:first_num]
+    @second_num = params[:second_num]
+    @result = @first_num / @second_num
+
+    render({ :template => "calculator/results_divide.html.erb" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,15 @@
 Rails.application.routes.draw do
+  get("/add", { :controller => "application", :action => "input_add" })
+  get("/wizard_add", { :controller => "application", :action => "wizard_add" })
+
+  get("/subtract", { :controller => "application", :action => "input_subtract" })
+  get("/wizard_subtract", { :controller => "application", :action => "wizard_subtract" })
+
+  get("/multiply", { :controller => "application", :action => "input_multiply" })
+  get("/wizard_multiply", { :controller => "application", :action => "wizard_multiply" })
+
+  get("/divide", { :controller => "application", :action => "input_divide" })
+  get("/wizard_divide", { :controller => "application", :action => "wizard_divide" })
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
